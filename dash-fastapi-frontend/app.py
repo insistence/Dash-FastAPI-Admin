b346cb95c91074f7fd7e132bd751a0ae1f18adbd
insistence <3055204202@qq.com>
2023-10-20 11:03:18 +0800

    perf:优化前端开发者模式下的默认启动方式

--- dash-fastapi-frontend/app.py
+++ dash-fastapi-frontend/app.py
@@ -255,4 +255,4 @@ def router(pathname, url_trigger, session_token):
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8088, debug=True)
+    app.run(host='0.0.0.0', port=8088, debug=True)
