a6297fb1e7c659854465358b8a833a8b9bd63f90
insistence <3055204202@qq.com>
2024-09-04 17:57:10 +0800

    chore: 调整跨域设置

--- dash-fastapi-backend/middlewares/cors_middleware.py
+++ dash-fastapi-backend/middlewares/cors_middleware.py
@@ -11,8 +11,8 @@ def add_cors_middleware(app: FastAPI):
     """
     # 前端页面url
     origins = [
-        'http://localhost:80',
-        'http://127.0.0.1:80',
+        'http://localhost:8088',
+        'http://127.0.0.1:8088',
     ]
 
     # 后台api允许跨域
