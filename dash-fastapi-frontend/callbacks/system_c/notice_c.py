8bc7b83f1237f95a2930f6872682967cff47b18a
insistence <3055204202@qq.com>
2024-09-19 20:01:08 +0800

    perf: 调整通知管理模块根据选择的表格数据行数控制修改和删除按钮状态回调为浏览器端回调

--- dash-fastapi-frontend/callbacks/system_c/notice_c.py
+++ dash-fastapi-frontend/callbacks/system_c/notice_c.py
@@ -158,45 +158,44 @@ app.clientside_callback(
 )
 
 
-@app.callback(
+# 根据选择的表格数据行数控制修改按钮状态回调
+app.clientside_callback(
+    """
+    (table_rows_selected) => {
+        outputs_list = window.dash_clientside.callback_context.outputs_list;
+        if (outputs_list) {
+            if (table_rows_selected?.length === 1) {
+                return false;
+            }
+            return true;
+        }
+        throw window.dash_clientside.PreventUpdate;
+    }
+    """,
     Output({'type': 'notice-operation-button', 'index': 'edit'}, 'disabled'),
     Input('notice-list-table', 'selectedRowKeys'),
     prevent_initial_call=True,
 )
-def change_notice_edit_button_status(table_rows_selected):
+
+
+# 根据选择的表格数据行数控制删除按钮状态回调
+app.clientside_callback(
     """
-    根据选择的表格数据行数控制编辑按钮状态回调
-    """
-    outputs_list = ctx.outputs_list
-    if outputs_list:
-        if table_rows_selected:
-            if len(table_rows_selected) > 1:
-                return True
-
-            return False
-
-        return True
-
-    raise PreventUpdate
-
-
-@app.callback(
+    (table_rows_selected) => {
+        outputs_list = window.dash_clientside.callback_context.outputs_list;
+        if (outputs_list) {
+            if (table_rows_selected?.length > 0) {
+                return false;
+            }
+            return true;
+        }
+        throw window.dash_clientside.PreventUpdate;
+    }
+    """,
     Output({'type': 'notice-operation-button', 'index': 'delete'}, 'disabled'),
     Input('notice-list-table', 'selectedRowKeys'),
     prevent_initial_call=True,
 )
-def change_notice_delete_button_status(table_rows_selected):
-    """
-    根据选择的表格数据行数控制删除按钮状态回调
-    """
-    outputs_list = ctx.outputs_list
-    if outputs_list:
-        if table_rows_selected:
-            return False
-
-        return True
-
-    raise PreventUpdate
 
 
 @app.callback(
