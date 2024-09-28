50644ea23d48ab126895205cba0cbe94bc09d8a8
insistence <3055204202@qq.com>
2024-09-28 22:41:59 +0800

    feat: 新增TTL缓存管理器

--- dash-fastapi-frontend/utils/cache_util.py
+++ dash-fastapi-frontend/utils/cache_util.py
@@ -1,9 +1,10 @@
-from cachebox import LRUCache
+from cachebox import LRUCache, TTLCache
 from flask import session
-from typing import Dict
+from typing import Any, Dict
 
 
 cache_manager = LRUCache(maxsize=10000, iterable=None, capacity=10000)
+ttl_manager = TTLCache(maxsize=0, ttl=600)
 
 
 class CacheManager:
@@ -52,3 +53,42 @@ class CacheManager:
         cache = cache_manager.get(session.get('Authorization'))
         cache.pop(target_key, None)
         cache_manager.insert(session.get('Authorization'), cache)
+
+
+class TTLCacheManager:
+    """
+    TTL缓存管理器
+    """
+
+    @classmethod
+    def get(cls, target_key: str):
+        """
+        获取缓存值
+
+        :param target_key: 缓存key
+        :return: 缓存值
+        """
+        return ttl_manager.get(target_key)
+
+    @classmethod
+    def set(cls, target_key: str, target_value: Any):
+        """
+        设置缓存值
+
+        :param target_key: 缓存key
+        :param target_value: 缓存值
+        :return:
+        """
+        ttl_manager.insert(target_key, target_value)
+
+    @classmethod
+    def delete(cls, target_keys: str):
+        """
+        删除缓存值
+
+        :param target_keys: 缓存keys
+        :return:
+        """
+        target_key_list = target_keys.split(',')
+        for target_key in target_key_list:
+            del ttl_manager[target_key]
